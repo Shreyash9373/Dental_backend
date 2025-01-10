45e9567fd181d487088f9a3e15eaffca7740beb3
shreyas <shreyashraut8@gmail.com>
2025-01-10 15:48:33 +0530

    Date type changed

--- backend/models/enquriySchema.js
+++ backend/models/enquriySchema.js
@@ -1,43 +1,41 @@
 import mongoose from "mongoose";
 
-const enquiry = new mongoose.Schema({
-  fullName: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    lowercase: true,
-    validate: {
-      validator: function (v) {
-        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(v);
+const enquiry = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+      validate: {
+        validator: function (v) {
+          return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(v);
+        },
+        message: (props) => `${props.value} is not a valid email!`,
       },
-      message: (props) => `${props.value} is not a valid email!`,
+    },
+    phone: {
+      type: String,
+      required: true,
+      validate: {
+        validator: function (v) {
+          return /^\d{10}$/.test(v);
+        },
+        message: (props) => `${props.value} is not a valid phone number!`,
+      },
+    },
+    message: {
+      type: String,
+      trim: true,
     },
   },
-  phoneNumber: {
-    type: String,
-    required: true,
-    validate: {
-      validator: function (v) {
-        return /^\d{10}$/.test(v);
-      },
-      message: (props) => `${props.value} is not a valid phone number!`,
-    },
-  },
-  problem: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  { timestamps: true }
+);
 
 const enquirySchema = mongoose.model("Enquiry", enquiry);
 
--- backend/routes/receptionRoute.js
+++ backend/routes/receptionRoute.js
@@ -2,10 +2,14 @@ import express from "express";
 import {
   bookAppointment,
   getPatient,
+  getEnquiry,
+  updatePatient,
 } from "../controllers/receptionController.js";
 const receptionRouter = express.Router();
 
 receptionRouter.post("/book-appointment", bookAppointment);
-receptionRouter.get("/get-patient", getPatient);
+receptionRouter.post("/get-patient", getPatient);
+receptionRouter.get("/get-Enquiry", getEnquiry);
+receptionRouter.post("/update-patient", updatePatient);
 
 export default receptionRouter;
