7e73f03abf9a1457f85b54defef14041850f12eb
smit <smitbharshankar110@gmail.com>
2025-01-15 17:28:54 +0530

    added asynchandler and error handler

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,8 @@ import receptionRouter from "./routes/receptionRoute.js";
 import patientRouter from "./routes/patientRoute.js";
 import cookieParser from "cookie-parser";
 import dashboardRouter from "./routes/dashboardRoute.js";
+import { errorHandler } from "./middlewares/error.middleware.js";
+
 
 import doctorRouter from "./routes/doctorRoute.js";
 
@@ -26,6 +28,9 @@ app.use("/api/reception", receptionRouter);
 app.use("/api/doctor", doctorRouter);
 app.use("/api/patient", patientRouter);
 app.use("/api/dashboard", dashboardRouter);
+
+app.use(errorHandler);
+
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
