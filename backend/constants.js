290f1c4fd4e11b2eaa87734cc9f43084e906e47e
smit <smitbharshankar110@gmail.com>
2025-01-22 12:32:27 +0530

    created new constant helper function and added maxAge to cookies

---
+++ backend/constants.js
@@ -0,0 +1,22 @@
+// cookieOptions.js
+
+const cookieOptions = (tokenType) => {
+    if (!['access', 'refresh'].includes(tokenType)) {
+      throw new Error("Invalid token type. Must be 'access' or 'refresh'.");
+    }
+  
+    const maxAgeValues = {
+      access: 15 * 60 * 1000, // 15 minutes in milliseconds
+      refresh: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
+    };
+  
+    return {
+      httpOnly: true, // Ensures the cookie is only accessible via HTTP(S), not JavaScript
+      secure: process.env.NODE_ENV === 'production', // Sends cookie over HTTPS in production
+      sameSite: 'strict', // Prevents CSRF attacks
+      maxAge: maxAgeValues[tokenType], // Sets the expiration time based on the token type
+    };
+  };
+  
+   export default cookieOptions;
+  
