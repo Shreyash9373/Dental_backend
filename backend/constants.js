66eefac2f3d8fe8e954e80a877753dfc810922c7
smit <smitbharshankar110@gmail.com>
2025-01-28 15:33:13 +0530

    removed maxAge from cookies

--- backend/constants.js
+++ backend/constants.js
@@ -1,20 +1,21 @@
 // cookieOptions.js
+const maxAgeValues = {
+  access: 15 * 60 * 1000, // 15 minutes in milliseconds
+  refresh: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
+};
 
 const cookieOptions = (tokenType) => {
     if (!['access', 'refresh'].includes(tokenType)) {
       throw new Error("Invalid token type. Must be 'access' or 'refresh'.");
     }
   
-    const maxAgeValues = {
-      access: 15 * 60 * 1000, // 15 minutes in milliseconds
-      refresh: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
-    };
+    
   
     return {
       httpOnly: true, // Ensures the cookie is only accessible via HTTP(S), not JavaScript
       secure: true, // Sends cookie over HTTPS in production
       sameSite: 'lax', // Prevents CSRF attacks
-      maxAge: maxAgeValues[tokenType], // Sets the expiration time based on the token type
+      // maxAge: maxAgeValues[tokenType], // Sets the expiration time based on the token type
     };
   };
   
